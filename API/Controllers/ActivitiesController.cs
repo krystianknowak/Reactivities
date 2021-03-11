9600d09677a4d3e607d44182234387041781bc9b
Krystian Nowak <krystian.k.nowak@gmail.com>
2021-03-11 19:47:08 +0100

    not working activity details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,31 +3,23 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
-  public class ActivitiesController : BaseApiController
-  {
-    private readonly IMediator _mediator;
-    public ActivitiesController(IMediator mediator)
+    public class ActivitiesController : BaseApiController
     {
-      _mediator = mediator;
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<List<Activity>>> getActivities()
-    {
-      return await _mediator.Send(new List.Query());
-    }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> getActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> getActivity(Guid id)
-    {
-      return Ok();
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> getActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{ Id = id });
+        }
     }
-  }
 }
