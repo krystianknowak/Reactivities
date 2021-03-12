f887cde014c83fa28344132b31687b7860e66747
Krystian Nowak <knowak@future-processing.com>
2021-03-12 16:23:05 +0100

    add create activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,19 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ActivitiesController : BaseApiController
+  public class ActivitiesController : BaseApiController
+  {
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
-        {
-            return await Mediator.Send(new List.Query());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await Mediator.Send(new Details.Query{Id = id});
-        }
+      return await Mediator.Send(new List.Query());
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity activity)
+    {
+      return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+    }
+  }
 }
