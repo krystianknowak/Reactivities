1cda89aa8c6014097a1245f2e6446e01150a771c
Krystian Nowak <knowak@future-processing.com>
2021-03-24 16:36:15 +0100

    handle result in base api controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
+using Application.Core;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,9 +18,9 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    public async Task<IActionResult> GetActivity(Guid id)
     {
-      return await Mediator.Send(new Details.Query { Id = id });
+      return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
     }
 
     [HttpPost]
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,14 +1,24 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BaseApiController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class BaseApiController : ControllerBase
+  {
+    private IMediator _mediator;
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    protected ActionResult HandleResult<T>(Result<T> result)
     {
-        private IMediator _mediator;
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+      if (result.IsSuccess && result.Value != null)
+        return Ok(result.Value);
+      if (result.IsSuccess && result.Value == null)
+        return NotFound();
+      return BadRequest(result.Error);
     }
+  }
 }
